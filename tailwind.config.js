b67cf86bb01a38be7c75fb7df6cd6146c676d2ad
Muhamad Husni <unny.husni@gmail.com>
2022-01-04 01:20:41 +0700

    am i good enough?

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,25 @@ module.exports = {
       fontFamily: {
         'inter': ['Inter', 'sans-serif']
       },
-
+      colors: {
+        'cream': '#fbf8f3',
+        'light-blue': '#d7f5f5'
+      },
     },
     container: {
       screens: {
-        lg:"1124px",
-        xl:"1124px",
-        "2xl":"1124px"
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
       },
       padding: "1rem",
       center: true
     }
   },
   variants: {
-    extend: {},
+    extend: {
+      fontWeight: ['hover']
+    },
   },
   plugins: [],
 }
